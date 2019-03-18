db78e027d273e3d14869fa102fade14d4613bb2c
emerson_kiefer <emerson_kiefer@student.wayland.k12.ma.us>
2019-03-18 09:53:12 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+
+// Create 2d array of zeros (the game field) that can be populated with pieces.
+//create piece permutations (four rotations of pieces)
+//
