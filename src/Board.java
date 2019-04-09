ccf110138d8ecac748a02facd33876362a98a573
emerson_kiefer <emerson_kiefer@student.wayland.k12.ma.us>
2019-04-09 09:51:58 -0400

    Fixed drawing live tetris boxes

--- src/Board.java
+++ src/Board.java
@@ -5,6 +5,7 @@ public class Board {
 
     public Board(){
         board[10][3] = 1; //set random x,y to one for testing.
+        board[10][4] =  2;
 
 //        for (int i = 0; i < board.length; i++) {
 //            for (int j = 0; j < board[0].length; j++) {
